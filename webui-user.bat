d3842e63777b612a3750b8bc806b514918ce713f
Vo1dAbyss <153193919+Vo1dAbyss@users.noreply.github.com>
2024-02-12 22:55:22 +0100

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,13 +1,6 @@
 @echo off
-title DialoGPT-medium-GPT4
+title DialoGPT-medium-GPT4 webui-user.bat
 color 0F
-echo ------------ DOWNLOADING GIT REPOSITORY ------------
-echo from time import sleep; sleep(1) | python
-git clone https://github.com/Vo1dAbyss/DialoGPT-medium-GPT4-Interface-Files.git
-cd DialoGPT-medium-GPT4-Interface-Files
-echo ------------ INSTALLING REQUIREMENTS... ------------
-echo from time import sleep; sleep(1) | python
-pip install -r requirements.txt
 echo ------------ LAUNCHING WEBUI.PY... -----------------
 echo from time import sleep; sleep(1) | python
 "Python" "webui.py"
